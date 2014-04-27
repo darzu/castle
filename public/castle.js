f9fcd997e32cfcaa67fcd661698dea25a7959b65
darzu <daryl.zuniga@gmail.com>
2014-04-27 11:51:06 -0700

    Updated castle drawing

--- public/castle.js
+++ public/castle.js
@@ -1,7 +1,11 @@
 var canvasElement = document.getElementById("castle-canvas");
 var canvas = canvasElement.getContext('2d');
 
-  console.log("Starting the castle game.");
+console.log("Starting the castle game.");
 
-  canvas.fillStyle = "#0000";//Black
-  canvas.fillRect(0, 0, 100, 100);
+canvas.fillStyle = "rgb(200,0,0)";
+canvas.fillRect(100, 20, 100, 10);
+
+canvas.font = "bold 20px sans-serif";
+
+canvas.fillText("FOO", 10, 10);
