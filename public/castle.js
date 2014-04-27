4017c0e231aa607e78bfd35dd19ef6b4b5ce3a50
darzu <daryl.zuniga@gmail.com>
2014-04-27 11:28:45 -0700

    updated castle with a canvas

---
+++ public/castle.js
@@ -0,0 +1,7 @@
+var canvasElement = document.getElementById("castle-canvas");
+var canvas = canvasElement.getContext('2d');
+
+  console.log("Starting the castle game.");
+
+  canvas.fillStyle = "#0000";//Black
+  canvas.fillRect(0, 0, 100, 100);
