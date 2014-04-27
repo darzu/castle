5e76045956aac9a2bb3b03927e25c7e6d29c6576
darzu <daryl.zuniga@gmail.com>
2014-04-27 12:20:51 -0700

    Updated castle with castle, enemy and arrows

--- public/castle.js
+++ public/castle.js
@@ -3,9 +3,55 @@ var canvas = canvasElement.getContext('2d');
 
 console.log("Starting the castle game.");
 
-canvas.fillStyle = "rgb(200,0,0)";
-canvas.fillRect(100, 20, 100, 10);
+var clear = function() {
+  canvas.fillStyle = "rgb(255, 255, 255)";
+  canvas.fillRect(0,0, 1000, 100);
+};
 
-canvas.font = "bold 20px sans-serif";
+var drawCastle = function() {
 
-canvas.fillText("FOO", 10, 10);
+  var castleChar = "Ħ";
+  var castleColor = "rgb(200,0,0)";
+  var castleFont = "bold 100 sans-serif";
+  var castleY = 100;
+  var castleX = 10;
+
+  canvas.fillStyle = castleColor;
+  canvas.font = castleFont;
+
+  canvas.fillText(castleChar, castleX, castleY);
+};
+
+var drawEnemy = function(x) {
+
+  var enemyChar = "♞";
+  var enemyColor = "rgb(200,0,0)";
+  var enemyFont = "bold 50 sans-serif";
+  var enemyY = 100;
+
+  canvas.fillStyle = enemyColor;
+  canvas.font = enemyFont;
+
+  canvas.fillText(enemyChar, x, enemyY);  
+
+};
+
+var drawArrow = function(x) {
+
+  var arrowChar = "☀";
+  var arrowColor = "rgb(200,0,0)";
+  var arrowFont = "bold 20 sans-serif";
+  var arrowY = 85;
+
+  canvas.fillStyle = arrowColor;
+  canvas.font = arrowFont;
+
+  canvas.fillText(arrowChar, x, arrowY);  
+
+};
+
+drawCastle();
+
+drawEnemy(800);
+
+drawArrow(500);
